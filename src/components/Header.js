6061c9953e5e5866cd27cb4d3c46fc7a6a2b3086
chrisstruong <chrisstruong@gmail.com>
2023-03-27 14:25:04 -0400

    Finish set up for front end

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,11 @@ function Header(props) {
         <header style={{ height: "480px", overflow: 'hidden' }}>
             <nav className="nav">
                 <Link to="/">
-                    <img style={{ width: "100%" }} src="https://media.istockphoto.com/photos/group-portrait-of-a-creative-business-team-standing-outdoors-three-picture-id1146473249?b=1&k=20&m=1146473249&s=612x612&w=0&h=-q1guVCuei7X3BFKwWC2bLUOX8BeIaC04pG5s_xfn_c=" />
+                    <img style={{ width: "100%" }} src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" />
                 </Link>
                 <div>People App</div>
             </nav>
-            <img style={{width:"100%"}} src="...your header image"/>
+            <img style={{width:"100%"}} src="https://media.istockphoto.com/photos/group-portrait-of-a-creative-business-team-standing-outdoors-three-picture-id1146473249?b=1&k=20&m=1146473249&s=612x612&w=0&h=-q1guVCuei7X3BFKwWC2bLUOX8BeIaC04pG5s_xfn_c="/>
         </header>
     )
 }
--- src/pages/People.js
+++ src/pages/People.js
@@ -1,8 +1,53 @@
-function People (props) {
+import { useState, useEffect } from "react"
+
+
+function People(props) {
+    const [people, setPeople] = useState([])
+
+    const BASE_URL = "http://localhost:4000/people"
+
+    const getPeople = async () => {
+        try {
+            const response = await fetch(BASE_URL)
+            const allPeople = await response.json()
+            setPeople(allPeople)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    console.log(`There are ${people.length} people available to render`)
+
+
+    useEffect(() => {
+        getPeople()
+    }, [])
+
+    const loaded = () => {
+        return people?.map((person) => {
+            return (
+                <div key={person._id}>
+                    <h1>{person.name}</h1>
+                    <img src={person.image} />
+                    <h3>{person.title}</h3>
+                </div>
+            )
+        })
+    }
+
+    const loading = () => {
+        <section className="people-list">
+            <h1>Loading...
+                <span>
+                    <img className="spinner" src="https://freesvg.org/img/1544764567.png" />{" "}
+                </span>
+            </h1>
+        </section>
+    }
+
     return (
-        <>
-        <h1>People page</h1>
-        </>
+        <section className="people-list">
+            {people && people.length ? loaded() : loading()}
+        </section>
     )
 }
 
